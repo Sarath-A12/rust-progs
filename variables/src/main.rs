172d7f2857f545c4d3f2cd7aa0a0469bd18f1986
Sarath <sarathas944609sa@gmail.com>
2026-02-09 23:42:51 +0530

    Var part2

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,7 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60*60*3;
+//Needs to be set at compile time, and type specified
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
